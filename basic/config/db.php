d82694ccef69ee3e4e58c7e70c8af99e38ef12a2
Luismi <luismiguelml@usal.es>
2021-12-22 17:40:24 +0100

    modifico db.php y borro .htaccess

--- basic/config/db.php
+++ basic/config/db.php
@@ -1,8 +1,8 @@
 <?php
 //Descomenta la línea que se corresponde con tu proyecto de trabajo.
 
-return require( 'db_proyecto_01_recetas_cocina.php');
-//return require( 'db_proyecto_02_actividades_y_eventos.php');
+//return require( 'db_proyecto_01_recetas_cocina.php');
+return require( 'db_proyecto_02_actividades_y_eventos.php');
 //return require( 'db_proyecto_03_alertas_ciudad.php');
 ?>
 <pre>
