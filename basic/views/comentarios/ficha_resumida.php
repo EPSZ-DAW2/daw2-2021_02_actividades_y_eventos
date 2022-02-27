edb5969fe9269f9565bc9402940537ad8ae81bd7
teresahdez <teresahernan98@gmail.com>
2022-02-27 21:30:38 +0100

    quitar delete de comentarios

--- basic/views/comentarios/ficha_resumida.php
+++ basic/views/comentarios/ficha_resumida.php
@@ -39,7 +39,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
             [
                 'class' => 'yii\grid\ActionColumn',
-                'template'=>'{view} {delete}',
+                'template'=>'{view}',
             ],
         ]
     ]); ?>
